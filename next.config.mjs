0f41eda80a59f4dfad55fb131f36e55e084a63d4
Nathaniel Lybrand <savorsauce@gmail.com>
2024-11-30 15:35:16 -0500

    Edited Package.json again

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -8,20 +8,21 @@ const nextConfig = {
         hostname: 'pub-8980b84e85064815a98217020b69d4c0.r2.dev',
       },
     ],
+    unoptimized: true, // Disable Image Optimization API
   },
   async headers() {
     return [
       {
-        source: "/(.*)",  // Apply to all paths
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace '*' with the actual origin if needed
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+        ],
+      },
     ];
-  }
-}
+  },
+};
 
 export default nextConfig;
